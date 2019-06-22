481c4af9de04bddce0a03f449494c0da52429926
刘志华 <2509687281@qq.com>
2019-06-23 00:15:51 +0800

    初始化仓库

---
+++ src/cn/xxx/Hello.java
@@ -0,0 +1,20 @@
+package cn.xxx;
+
+/**
+ * lzhh
+ * 2019/6/22 21:58
+ * 类说明：
+ */
+
+
+public class Hello {
+    /**
+     * main方法程序的主入口
+     * @param args
+     */
+    public static void main(String[] args) {
+        String name ="xiaolinzi";
+        System.out.println("I LOVE YOU " + name);
+
+    }
+}
